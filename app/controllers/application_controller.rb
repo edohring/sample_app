e0d044bf2f64da3e7e16d305b3c36853f9b76be2
Erica Dohring <erica.dohring@wework.com>
2017-06-02 08:04:09 -0400

    Adding some flavoring

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  def greet
+    render html: "Greetings and Permutations"
+  end
+
   def hello
     render html: "hey"
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  get 'static_pages/home'
+  # Defines correspondence between the URLs and web pages 
+  #root 'static_pages#home'
+  root 'application#greet'
+  get 'static_pages/home'# maps requests for url/static_pages/home to static pages controller - respond to a get request 
+  # when we generate a home action inside the static pages controller we get stuff 
   get 'static_pages/help'
   get 'static_pages/about'
 end
